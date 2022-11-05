e226228c4493a22034747fe5e0711b2ca4fd0a9e
uiseop <bono521@naver.com>
2022-11-06 00:57:58 +0900

    FE 간단 로그인 화면 구현
    로그인 로직에는 오직 이름만 받기로..
    form 제작에 있어 번거로운 작업은 덜어내기 위해 react-hook-form을 사용
    styled-components 대신 css framework를 사용함으로써 디자인 시간 줄이기

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,24 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import Router from "./Router";
 
+const colors = {
+    brand: {
+        900: "#1a365d",
+        800: "#153e75",
+        700: "#2a69ac",
+    },
+};
+
+const theme = extendTheme({ colors });
+
 const App = () => {
     return (
         <>
-            <Router />
-            <GlobalStyles />
+            <ChakraProvider theme={theme}>
+                <Router />
+            </ChakraProvider>
         </>
     );
 };
--- front/src/Pages/Login.tsx
+++ front/src/Pages/Login.tsx
@@ -1,5 +1,72 @@
+import {
+    Button,
+    FormControl,
+    FormErrorMessage,
+    FormLabel,
+    Input,
+    useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import styled from "styled-components";
+
+interface IFormValues {
+    name: string;
+}
+
 const Login = () => {
-    return <h1>Hello This is Login Page</h1>;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<IFormValues>();
+    const toast = useToast();
+
+    const onSubmit: SubmitHandler<IFormValues> = (data) => {
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                toast({
+                    title: `로그인이 완료되었습니다!`,
+                    description: `${data.name}님 환영합니다!`,
+                    status: 'success',
+                    duration: 3000,
+                    isClosable: true
+                });
+                resolve();
+            }, 1500);
+        });
+    };
+    return (
+        <>
+            <LoginForm onSubmit={handleSubmit(onSubmit)}>
+                <FormControl>
+                    <FormLabel htmlFor="name">이름</FormLabel>
+                    <Input
+                        id="name"
+                        placeholder="name"
+                        {...register("name", { required: "이름은 필수입니다" })}
+                    />
+                    <FormErrorMessage>
+                        {errors.name && errors.name.message}
+                    </FormErrorMessage>
+                </FormControl>
+                <Button
+                    mt={4}
+                    colorScheme="teal"
+                    isLoading={isSubmitting}
+                    type="submit"
+                >
+                    입장하기
+                </Button>
+            </LoginForm>
+        </>
+    );
 };
+
+const LoginForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+`;
 
 export default Login;
