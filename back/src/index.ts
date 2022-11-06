eada31f812e2c733087ac64d7ebb5fb7be9e4715
uiseop <bono521@naver.com>
2022-11-06 21:22:30 +0900

    BE Express의 특징과 미들웨어의 동작 과정

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,14 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("hello this is middleware");
+    next()
+});
+
+app.use((req, res, next) => {
+    console.log("another middleware")
+})
+
 app.get("/", (req, res, next) => {
     res.send("Hi This is my First Express Server");
 });
 
-app.listen('8000', () => {
+app.listen("8000", () => {
     console.log(`
     #############################################
         🛡️ Server listening on port: 8000 🛡️
     #############################################      
-    `)
-})
+    `);
+});
--- front/src/Pages/Login.tsx
+++ front/src/Pages/Login.tsx
@@ -6,7 +6,6 @@ import {
     Input,
     useToast,
 } from "@chakra-ui/react";
-import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import styled from "styled-components";
 
@@ -28,45 +27,45 @@ const Login = () => {
                 toast({
                     title: `로그인이 완료되었습니다!`,
                     description: `${data.name}님 환영합니다!`,
-                    status: 'success',
+                    status: "success",
                     duration: 3000,
-                    isClosable: true
+                    isClosable: true,
                 });
                 resolve();
             }, 1500);
         });
     };
+
     return (
-        <>
-            <LoginForm onSubmit={handleSubmit(onSubmit)}>
-                <FormControl>
-                    <FormLabel htmlFor="name">이름</FormLabel>
-                    <Input
-                        id="name"
-                        placeholder="name"
-                        {...register("name", { required: "이름은 필수입니다" })}
-                    />
-                    <FormErrorMessage>
-                        {errors.name && errors.name.message}
-                    </FormErrorMessage>
-                </FormControl>
-                <Button
-                    mt={4}
-                    colorScheme="teal"
-                    isLoading={isSubmitting}
-                    type="submit"
-                >
-                    입장하기
-                </Button>
-            </LoginForm>
-        </>
+        <LoginForm onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors.name?.type === "required"}>
+                <FormLabel htmlFor="name">이름</FormLabel>
+                <Input
+                    id="name"
+                    placeholder="name"
+                    {...register("name", { required: true })}
+                />
+
+                {errors.name?.type === "required" && (
+                    <FormErrorMessage>이름은 필수입니다</FormErrorMessage>
+                )}
+            </FormControl>
+
+            <Button
+                mt={4}
+                colorScheme="teal"
+                isLoading={isSubmitting}
+                type="submit"
+            >
+                입장하기
+            </Button>
+        </LoginForm>
     );
 };
 
 const LoginForm = styled.form`
     display: flex;
     flex-direction: column;
-    gap: 14px;
 `;
 
 export default Login;
