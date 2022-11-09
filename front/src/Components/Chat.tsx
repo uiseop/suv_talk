69bf0aeb6d471b3f9bf887710b26cd6e7c1c4cad
uiseop <bono521@naver.com>
2022-11-09 17:05:53 +0900

    FE 채팅 인터페이스 생성

--- front/src/Components/Chat.tsx
+++ front/src/Components/Chat.tsx
@@ -1,17 +1,79 @@
 import styled from "styled-components";
 
 const Chat = () => {
-    return <ChatWrapper></ChatWrapper>;
+    return (
+        <ChatWrapper>
+            <ChatHeader>
+                <CloseBtn />
+            </ChatHeader>
+            <ChatInner></ChatInner>
+            <ChatInputWrapper></ChatInputWrapper>
+        </ChatWrapper>
+    );
 };
 
 const ChatWrapper = styled.div`
     position: absolute;
+    display: flex;
+    flex-direction: column;
     margin: 1rem;
     background-color: #f3f0e7;
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
+`;
+
+const ChatHeader = styled.nav`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin: 0 15px;
+    height: 60px;
+`;
+
+const CloseBtn = styled.button`
+    position: relative;
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+
+    &::before,
+    &::after {
+        position: absolute;
+        top: 0;
+        content: " ";
+        height: 30px;
+        width: 2px;
+        background-color: #fff;
+    }
+
+    &:hover {
+        &::before,
+        &::after {
+            background-color: #ffeb3b;
+        }
+    }
+
+    &::before {
+        transform: rotate(45deg);
+    }
+
+    &::after {
+        transform: rotate(-45deg);
+    }
+`;
+
+const ChatInner = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #878787;
+    flex: 1;
+`;
+
+const ChatInputWrapper = styled.div`
+    background-color: #cecece;
+    height: 45px;
 `;
 
 export default Chat;
