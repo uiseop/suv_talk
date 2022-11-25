948d6a3face156ba38750110ee9926f513c9e7ba
uiseop <bono521@naver.com>
2022-11-25 21:03:28 +0900

    BE 회원가입 로직 생성

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
 import userRouter from "./routes/user";
 
 const app = express();
+app.use(express.json());
+// app.use(helmet())
+// app.use(morgan("common"))
 
 dotenv.config();
 
@@ -12,9 +17,10 @@ app.use("/api/user", userRouter);
 mongoose.connect(process.env.MONGO_URL, () => {
     console.log("Connected to MongoDB");
     app.listen(8000, () => {
-        console.log(`#############################################
+        console.log(`
+        #############################################
         🛡️ Server listening on port: 8000 🛡️
-    #############################################      
+        #############################################      
     `);
     });
 });
--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -3,17 +3,27 @@ import asyncHandler from "../middlewares/asyncHandler";
 import User from "../models/User";
 const userRouter = Router();
 
-userRouter.get(
-    "/register",
+userRouter.post(
+    "/signin",
     asyncHandler(async (req, res, next) => {
-        const user = await new User({
-            username: "seop",
-            profileImage:
-                "https://mongoosejs.com/docs/jobs#62c288992e788eb5404ba57d",
-        });
-        
-        await user.save()
-        res.json()
+        const { username } = req.body;
+        const isExist = await User.findOne({ username });
+        if (isExist) {
+            res.status(200).json({
+                msg: "기존 가입된 아이디로 로그인",
+                user: isExist,
+            });
+        } else {
+            const user = new User({
+                username,
+            });
+            await user.save();
+            res.status(201).json({
+                msg: "새로운 아이디 생성",
+                user,
+            });
+        }
+        res.json();
     })
 );
 
