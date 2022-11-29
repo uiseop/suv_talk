a8aea5ca5a7afb1333a1d8211cd966ff0842fe32
uiseop <bono521@naver.com>
2022-11-30 00:53:30 +0900

    Edit Routings, if user -> status.200 else status (301) -> redirect to login

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,14 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { IUserContext } from "./@types/user";
+import { UserContext } from "./Context/UserContext";
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
 const Router = () => {
+    const { user } = useContext(UserContext) as IUserContext;
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/profile/:username" element={<Profile />} />
+                <Route
+                    path="/"
+                    element={user ? <Home /> : <Navigate to={"/login"} />}
+                />
+                <Route
+                    path="/login"
+                    element={user ? <Navigate to={"/"} /> : <Login />}
+                />
+                <Route
+                    path="/profile/:username"
+                    element={user ? <Profile /> : <Navigate to={"/login"} />}
+                />
             </Routes>
         </BrowserRouter>
     );
